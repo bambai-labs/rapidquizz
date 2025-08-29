05da560193a64b7f19ae62bc2808061ab37b7d03
Marco Portacio <marcoportacio4@gmail.com>
2025-08-29 17:29:24 -0500

    fixed logout not working on production

--- modules/shared/hooks/useNavBar.ts
+++ modules/shared/hooks/useNavBar.ts
@@ -1,12 +1,11 @@
 import { logout } from '@/actions/auth'
 import { useAuthStore } from '@/stores/auth-store'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 import { useState } from 'react'
 import { toast } from 'sonner'
 
 export const useNavBar = () => {
   const [loading, setLoading] = useState(false)
-  const router = useRouter()
   const { user } = useAuthStore()
 
   const handleLogout = async () => {
@@ -20,7 +19,7 @@ export const useNavBar = () => {
     }
 
     setLoading(false)
-    router.replace('/')
+    redirect('/')
   }
 
   return {
