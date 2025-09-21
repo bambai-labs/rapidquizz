d905c9a6683188e6907a772aeb55b40dba66e9cf
Marco Portacio <marcoportacio4@gmail.com>
2025-09-21 17:18:41 -0500

    fix: cambiada ruta de callback para iniciar sesion con google

--- actions/auth.ts
+++ actions/auth.ts
@@ -89,7 +89,7 @@ export async function signInWithGoogle(): Promise<Result<void>> {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: `${window.location.origin}/auth/confirm/google`,
+      redirectTo: `${window.location.origin}/auth/callback/google`,
     },
   })
 
